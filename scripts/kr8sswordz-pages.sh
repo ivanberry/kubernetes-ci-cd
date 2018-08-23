b423802e896900c3d4db0c72beacc765485c8724
ivanberry <tabvim.dev@gmail.com>
2018-08-23 17:05:37 +0800

    Update registryHost

--- scripts/kr8sswordz-pages.sh
+++ scripts/kr8sswordz-pages.sh
@@ -4,7 +4,7 @@
 TAG=`git rev-parse --short HEAD`
 
 #Build the docker image
-docker build -t 127.0.0.1:30400/kr8sswordz:$TAG -f applications/kr8sswordz-pages/Dockerfile applications/kr8sswordz-pages
+docker build -t local.docker.cn:5000/kr8sswordz:$TAG -f applications/kr8sswordz-pages/Dockerfile applications/kr8sswordz-pages
 
 #Setup the proxy for the registry
 docker stop socat-registry; docker rm socat-registry; docker run -d -e "REGIP=`minikube ip`" --name socat-registry -p 30400:5000 chadmoon/socat:latest bash -c "socat TCP4-LISTEN:5000,fork,reuseaddr TCP4:`minikube ip`:30400"
@@ -13,10 +13,10 @@ echo "5 second sleep to make sure the registry is ready"
 sleep 5;
 
 #Push the images
-docker push 127.0.0.1:30400/kr8sswordz:$TAG
+docker push local.docker.cn:5000/kr8sswordz:$TAG
 
 #Stop the registry proxy
 docker stop socat-registry
 
 # Create the deployment and service for the front end aka kr8sswordz
-sed 's#127.0.0.1:30400/kr8sswordz:latest#127.0.0.1:30400/kr8sswordz:'$TAG'#' applications/kr8sswordz-pages/k8s/deployment.yaml | kubectl apply -f -
+sed 's#local.docker.cn:5000/kr8sswordz:latest#local.docker.cn:5000/kr8sswordz:'$TAG'#' applications/kr8sswordz-pages/k8s/deployment.yaml | kubectl apply -f -
--- scripts/monitor-scale.sh
+++ scripts/monitor-scale.sh
@@ -4,7 +4,7 @@
 TAG=`git rev-parse --short HEAD` 
 
 #Build the docker image
-docker build -t 127.0.0.1:30400/monitor-scale:$TAG -f applications/monitor-scale/Dockerfile applications/monitor-scale
+docker build -t local.docker.cn:5000/monitor-scale:$TAG -f applications/monitor-scale/Dockerfile applications/monitor-scale
 
 #Setup the proxy for the registry
 docker stop socat-registry; docker rm socat-registry; docker run -d -e "REGIP=`minikube ip`" --name socat-registry -p 30400:5000 chadmoon/socat:latest bash -c "socat TCP4-LISTEN:5000,fork,reuseaddr TCP4:`minikube ip`:30400"
@@ -13,10 +13,10 @@ echo "5 second sleep to make sure the registry is ready"
 sleep 5;
 
 #Push the images
-docker push 127.0.0.1:30400/monitor-scale:$TAG
+docker push local.docker.cn:5000/monitor-scale:$TAG
 
 #Stop the registry proxy
 docker stop socat-registry
 
 # Create the deployment and service for the monitor-scale node server
-sed 's#127.0.0.1:30400/monitor-scale:latest#127.0.0.1:30400/monitor-scale:'$TAG'#' applications/monitor-scale/k8s/deployment.yaml | kubectl apply -f -
+sed 's#local.docker.cn:5000/monitor-scale:latest#local.docker.cn:5000/monitor-scale:'$TAG'#' applications/monitor-scale/k8s/deployment.yaml | kubectl apply -f -
